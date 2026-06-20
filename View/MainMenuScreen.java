52c87d48c7e465c15896b65815e5a0cb96292bd7
Amir Khosravi <sam.khosravifard.85@sharif.edu>
2026-06-20 17:57:20 +0330

    exit button

--- View/MainMenuScreen.java
+++ View/MainMenuScreen.java
@@ -1,21 +1,45 @@
 package com.test.liftoff.View;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Stack;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class MainMenuScreen implements Screen {
+    private Stage stage;
+    private Skin skin;
     @Override
     public void show() {
+        stage = new Stage(new ScreenViewport());
+        skin = new Skin(Gdx.files.internal("uiSkin/plain-james-ui.json"));
+
+        Stack stack = new Stack();
+        stack.setFillParent(true);
+
+        Table exitButtonWrapper = new Table();
+        exitButtonWrapper.bottom().left().pad(10);
+        TextButton exitButton = new TextButton("exit", skin);
+        exitButtonWrapper.add(exitButton);
+
+        stack.add(exitButtonWrapper);
+
+        stage.addActor(stack);
+    }
+
+    @Override
+    public void render(float delta) {
+        stage.act(delta);
+        stage.draw();
 
     }
 
     @Override
-    public void render(float v) {
-
-    }
-
-    @Override
-    public void resize(int i, int i1) {
-
+    public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
     }
 
     @Override
